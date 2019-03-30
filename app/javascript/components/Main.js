f05e91285c3ee088f318cc2f0b6ad3c9c43d6db6
dianpan <dian.di.pan@gmail.com>
2019-03-30 11:27:19 -0700

    Use webpacker-react gem

---
+++ app/javascript/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Main = ({
+  fruitName
+}) => {
+  return(
+    <div>
+      <h1>{fruitName} are great!</h1>
+    </div>
+  )
+}
+
+export default Main
---
+++ app/javascript/packs/App.js
@@ -0,0 +1,7 @@
+import Main from '../components/Main'
+import WebpackerReact from 'webpacker-react'
+
+WebpackerReact.setup({Main}) // ES6 shorthand for {Hello: Hello}
+
+
+// app/javascript/components/hello.js:
