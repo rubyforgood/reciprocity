01685d1bb88926b69f11e0344cecd37b7f9f4474
dave doolin <david.doolin@gmail.com>
2019-03-29 20:56:39 -0700

    rubocop - get linting started early
    This can be hooked to automatically fail test
    runs, including on the ci.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-     resources :participants, only: [:index, :create, :destroy, :update]
+      resources :participants, only: [:index, :create, :destroy, :update]
     end
   end
 end
