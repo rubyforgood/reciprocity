cab0ccd89f09326a35c77d8b5b1a65c35d3fe676
Sara Bocciardi <sjbocciardi@gmail.com>
2019-03-30 10:20:11 -0700

    Add particpants API controller.

---
+++ app/controllers/api/v1/participants_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::FruitsController < ApplicationController
+  def index
+    render json: Participant.all
+  end
+
+  def create
+    participant = Participant.create(participant_params)
+    render json: participant
+  end
+
+  def destroy
+    Participant.destroy(params[:id])
+  end
+
+  def update
+    participant = Participant.find(params[:id])
+    participant.update_attributes(participant_params)
+    render json: participant
+  end
+
+  private
+
+  def participant_params
+    params.require(:participant).permit(:id, :display_name, :about_me)
+  end
+end
