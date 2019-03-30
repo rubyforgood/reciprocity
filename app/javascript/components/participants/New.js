e588b71486e0812ef610ec36f683a6512d061683
Sara Bocciardi <sjbocciardi@gmail.com>
2019-03-30 16:16:04 -0700

    Add components for new participant and render them.

---
+++ app/javascript/components/participants/New.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+const NewParticipant = () => {
+  return(
+    <form action="/participants" method="post">
+      <div className="form-group">
+        <label htmlFor="display_name">Your name</label>
+        <input name="display_name" className="form-control" id="display_name" aria-describedby="nameHelp" placeholder="Enter name" />
+        <small id="nameHelp" className="form-text text-muted">This is your display name in the application</small>
+      </div>
+      <div className="form-group">
+        <label htmlFor="zip_code">Your zip code</label>
+        <input name="zip_code" className="form-control" aria-describedby="zipCodeHelp" id="zip_code" />
+        <small id="zipCodeHelp" className="form-text text-muted">What is your home zipcode?</small>
+      </div>
+      <div className="form-group">
+        <label htmlFor="about_me">About me</label>
+        <textarea name="about_me" className="form-control" id="about_me" aria-describedby="aboutMeHelp" rows="3" />
+        <small id="aboutMeHelp" className="form-text text-muted">Tell us a bit about yourself</small>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  )
+}
+
+export default NewParticipant
+
