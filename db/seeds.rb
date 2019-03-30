90f166cda484d1563ea2a213e488a168ed2df4e5
Brantron <bvbrandonbv@gmail.com>
2019-03-30 16:32:36 -0700

    add to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
-user = User.new(email: 'reciprocity@love.com',
-             password: 'topsecret',
-             password_confirmation: 'topsecret')
+if User.where(email: 'reciprocity@love.com').blank?
+  user = User.new(email: 'reciprocity@love.com', password: 'topsecret', password_confirmation: 'topsecret')
+  user.skip_confirmation!
+  user.save!
+end
 
- user.skip_confirmation!
- user.save!
+10.times do |n|
+  Participant.create!(
+    display_name: "participant-#{n}",
+    about_me: "long bio about participant-#{n}",
+    zip_code: '94596'
+  )
+end
