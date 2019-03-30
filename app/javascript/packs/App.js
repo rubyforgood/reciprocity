edbf2fa34179684b1cbf1ff8ad7b996814b6abab
Sara Bocciardi <sjbocciardi@gmail.com>
2019-03-30 14:06:24 -0700

    Make app less fruit focused

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,7 +1,4 @@
 import Main from '../components/Main'
 import WebpackerReact from 'webpacker-react'
 
-WebpackerReact.setup({Main}) // ES6 shorthand for {Hello: Hello}
-
-
-// app/javascript/components/hello.js:
+WebpackerReact.setup({Main})
