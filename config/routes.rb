305358908d7ca6d335121b5bbb911f027e6206ec
Sara Bocciardi <sjbocciardi@gmail.com>
2019-03-30 10:22:31 -0700

    Add routes for participants

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/', to: 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+     resources :participants, only: [:index, :create, :destroy, :update]
+    end
+  end
 end
