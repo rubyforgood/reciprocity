93058b6f09c1fcf7b1b877874fb71fd27e371518
maebeale <maebeale@gmail.com>
2019-03-31 16:23:55 -0700

    Change to multiline for rubocop

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -68,6 +68,16 @@ class OrganizationsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def organization_params
-    params.require(:organization).permit(:name, :website_url, :twitter_username, :facebook_username, :instagram_username, :phone, :street_address, :city, :state, :zip, :description)
+    params.require(:organization).permit(:name,
+                                         :website_url,
+                                         :twitter_username,
+                                         :facebook_username,
+                                         :instagram_username,
+                                         :phone,
+                                         :street_address,
+                                         :city,
+                                         :state,
+                                         :zip,
+                                         :description)
   end
 end
