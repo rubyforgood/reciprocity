f642a49ac5a623d6e788700aa0650b1a98a148bc
dianpan <dian.di.pan@gmail.com>
2019-03-31 15:49:41 -0700

    Add positions CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :positions
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
---
+++ spec/factories/positions.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :position do
+    bio { 'MyText' }
+    start_date { '2019-03-31 15:29:34' }
+    end_date { '2019-03-31 15:29:34' }
+    show_on_team_page { false }
+  end
+end
