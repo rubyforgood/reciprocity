43b3bdadc917b7046f1beca11f6b26c6bb30ff6b
adina <adina.r.bianchi@gmail.com>
2019-04-01 13:30:58 -0400

    Create form to generate batch of invite codes

--- app/javascript/components/home/Show.js
+++ app/javascript/components/home/Show.js
@@ -3,14 +3,16 @@ import ListingForm from "../shared/ListingForm";
 import { ParticipantData } from "../participants/Show";
 import ListingsGrid from "../shared/ListingsGrid";
 import InviteForm from '../invitations/InviteForm'
+import CreateCodesForm from '../invitations/CreateCodesForm'
 import ReferralCodes from '../invitations/Index'
 
-const ShowDashboard = ({ form, listings, inviteForm, invitationCodes, ...props }) => {
+const ShowDashboard = ({ form, listings, inviteForm, invitationCodes, createCodesForm,  ...props }) => {
   return (
     <div className="row">
       <div className="col col-md-4">
         <ParticipantData {...props} />
         <InviteForm form={inviteForm} />
+        <CreateCodesForm form={createCodesForm} />
         <ReferralCodes invitationCodes={invitationCodes}/>
       </div>
       <div className="col col-md-4">
