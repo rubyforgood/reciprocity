95127676c85b71c81f2d4d3c337b234074fc6109
Eric Tillberg <eric.tillberg@gmail.com>
2019-08-04 13:37:57 -0400

    Add validations for Listings
    This commit adds presence validations for some fields on the Listing
    model. Issue #147 digs into a question raised by this commit: do we want
    to enforce presence of these fields on the database, too?
    
    Fixess #138

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,15 @@ class Listing < ApplicationRecord
   has_many_attached :photos
 
   acts_as_taggable_on :tags
+
+  validates(
+    :type,
+    :title,
+    :publish_start_date,
+    :publish_end_date,
+    :interaction_start_date,
+    :interaction_end_date,
+    :zip_code,
+    presence: true
+  )
 end
